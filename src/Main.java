4b841fea769e50697a6a280bf27a0847bab49693
osorov97 <osorov.nursultan97@gmail.com>
2022-09-03 17:03:47 +0600

    HW21efe

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,39 @@
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        try (FileWriter fileWriter = new FileWriter("san.txt")) {
+            for (char i = 'A'; i <= 'Z'; i++) {
+                String string = i + " " + Character.toLowerCase(i);
+                fileWriter.write(string + " \n");
+            }
+
+
+            for (char i = '0'; i <= '9'; i++) {
+                fileWriter.write(i + "\n");
+            }
+
+        }
+        try (FileReader fileReader = new FileReader("san.txt")) {
+            scanner = new Scanner(fileReader);
+            int counter = 1;
+            while (scanner.hasNextLine()) {
+                System.out.println(counter + ")" + scanner.nextLine());
+                counter++;
+
+            }
+
+        }
+
+
     }
+
+
 }
+
+
+
